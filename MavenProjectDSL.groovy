dbda6ab217d783ca2339432bdd56c5b40cc0bc30
wasi2320 <wasifsuhail23@gmail.com>
2022-06-10 02:00:59 -0500

    new commit

--- MavenProjectDSL.groovy
+++ MavenProjectDSL.groovy
@@ -2,13 +2,13 @@ job("First-Maven-Project-Via-DSL_1")
 {
     description("First maven jonb using code dsl on ${new Date()}")
     scm{
-        git("https://github.com/wasi2320/anshulc55_Jenkins_Upgradev3.git",master)
+        git("https://github.com/wasi2320/anshulc55_Jenkins_Upgradev3.git", master)
     }
     triggers{
         scm('* * * * *')
     }
     steps{
-        maven("clear package",'maven-samples/single-moule/pom.xml',master)
+        maven("clear package",'maven-samples/single-moule/pom.xml')
     }
     publishers{
         archiveArtifacts '**/*.jar'
